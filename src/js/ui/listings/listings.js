7b728bab4387c515ec75b3f0659a15d46a5d1e39
Ingrid Ornum <ingrid.ornum@gmail.com>
2024-12-12 13:27:34 +0100

    Chore: added styling for nav bar. Added styling for cards.

--- src/js/ui/listings/listings.js
+++ src/js/ui/listings/listings.js
@@ -1,5 +1,6 @@
 import {searchListings} from "../../api/listings/search.js";
 import {readAllListings} from "../../api/listings/read.js";
+import {getHighestBid} from "../single-listing/single-listing.js";
 
 let currentSearchData = '';
 let currentPage = 0;
@@ -90,27 +91,31 @@ export async function loadListings(page = 1, searchData = '', sortOption, filter
 export async function displayListings(listings) {
 
     const listingsContainer = document.querySelector('.listings-container');
-    const listingsCount = document.getElementById('listings-count');
+    const listingsCount = document.querySelectorAll('.listings-count');
     listingsContainer.innerHTML = '';
 
     if (listings.length === 0) {
         listingsContainer.innerHTML = '<p>No listings found.</p>';
-        listingsCount.innerHTML = '0 of 0 listings'
+        listingsCount.forEach(counter => {
+            counter.innerHTML = '0 of 0 listings'
+        })
         return;
     }
 
-    listingsCount.innerHTML = `
+    listingsCount.forEach(counter => {
+        counter.innerHTML = `
         <span>${listings.length} of ${metaData.totalCount}</span>
         `;
+    });
 
     for (const listing of listings) {
-        const formattedTitle = listing.title.length > 10 ? `${listing.title.substring(0, 10)}...` : listing.title;
         const currentTime = new Date();
         const auctionEndTime = new Date(listing.endsAt);
         const hasAuctionEnded = currentTime > auctionEndTime;
         const timeDiff = auctionEndTime - currentTime;
         const days = Math.floor(timeDiff / (100 * 60 * 60 * 24));
         const hours = Math.floor((timeDiff % ((100 * 60 * 60 * 24))) / (1000 * 60 * 60));
+        const highestBid = await getHighestBid(listing.bids);
 
         const timeRemaining = days > 0
             ? ` ${days} days & ${hours} hours`
@@ -121,36 +126,39 @@ export async function displayListings(listings) {
         listItem.setAttribute('data-id', listing.id);
 
         listItem.innerHTML = `
-            <div class="li-single-listing-content flex flex-col relative rounded-xl border-collapse">
-            <div>
-            ${hasAuctionEnded
-            ?
-            `<div id="ended-notif" class="font-text text-xs text-notif-red absolute m-3 top-0 right-0 px-2 py-1 border border-notif-red bg-notif-bg-red z-1 rounded-full tablet:text-base">ENDED</div>
-         `
-            :
-            `
-            <div id="active-notif" class=" font-text text-xs text-notif-green absolute m-3 top-0 right-0 px-2 py-1 border border-notif-green bg-notif-bg-green z-1 rounded-full tablet:text-base">ACTIVE</div>
-            `
-        }
-</div>
-            <div id="soon-notif" class="hidden font-text text-xs text-notif-yellow absolute m-3 top-0 right-0 px-2 py-1 border border-notif-yellow bg-notif-bg-yellow z-1 rounded-full">END SOON</div>
-            <div id="ended-notif" class="hidden font-text text-xs text-notif-red absolute m-3 top-0 right-0 px-2 py-1 border border-notif-red bg-notif-bg-red z-1 rounded-full">ENDED</div>
-                <img src="${listing.media?.[0]?.url || "public/assets/images/missing-img.jpeg"}" alt="${listing.media?.[0]?.alt || "No image"}">
-                <div class="flex flex-col gap-4 p-4 min-h-[112px]">
-                    <span class="font-subtitle text-ui-black text-lg tablet:text-2xl">${formattedTitle}</span>
-                    ${hasAuctionEnded
-                    ?
-            ` <span class="uppercase text-notif-red font-text text-xs tablet:text-base">Ended</span>`
-            :
-            `
-                <div class="flex flex-col font-text text-xs gap-1 font-light tablet:text-base tablet:flex-row">
-                    <span class="font-medium">Ends in:</span>
-                    <span> ${timeRemaining}</span>
+            <div class="li-single-listing-content flex flex-col relative rounded-xl">
+                <div class="flex items-center gap-4 p-2">
+                    <img class="rounded-full h-10 w-10 object-cover" src="${listing.seller.avatar.url || "public/assets/images/missing-img.jpeg"}" alt="Avatar User">
+                    <span>${listing.seller.name}</span>
                 </div>
-            </div>
-            `
-        }
-
+                <div>
+                    ${hasAuctionEnded
+                        ?
+                        `<div id="ended-notif" class="font-text text-xs text-notif-red absolute m-3 mt-[68px] top-0 right-0 px-2 py-1 border border-notif-red bg-notif-bg-red z-1 rounded-full tablet:text-base">ENDED</div>`
+                        :  
+                        `<div id="active-notif" class=" font-text text-xs text-notif-green absolute m-3 mt-[68px] top-0 right-0 px-2 py-1 border border-notif-green bg-notif-bg-green z-1 rounded-full tablet:text-base">ACTIVE</div>`
+                    }
+                </div>
+<!--                    <div id="soon-notif" class="hidden font-text text-xs text-notif-yellow absolute m-3 top-0 right-0 px-2 py-1 border border-notif-yellow bg-notif-bg-yellow z-1 rounded-full">END SOON</div>-->
+                        <img class="listing-img" src="${listing.media?.[0]?.url || "public/assets/images/missing-img.jpg"}" alt="${listing.media?.[0]?.alt || "No image"}">
+                        <div class="flex flex-col gap-4 p-4 min-h-[112px]">
+                        <span class="font-subtitle text-ui-black text-lg tablet:text-2xl overflow-hidden whitespace-nowrap max-w-full">${listing.title}</span>
+                        ${hasAuctionEnded
+                        ?
+                        ` <span class="uppercase text-notif-red font-text text-xs tablet:text-base">Ended</span>`
+                        :
+                        `
+                        <div class="flex flex-col font-text text-xs gap-1 font-light tablet:text-base tablet:flex-row">
+                            <span class="font-medium">Highest bid:</span>
+                            <span>${highestBid} credits</span>
+                        </div>
+                        <div class="flex flex-col font-text text-xs gap-1 font-light tablet:text-base tablet:flex-row">
+                            <span class="font-medium">Ends in:</span>
+                            <span> ${timeRemaining}</span>
+                        </div>
+                </div>
+                `
+                }
             </div>
         `;
         listingsContainer.appendChild(listItem);
